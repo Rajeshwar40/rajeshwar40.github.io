5f8c2ad408e12ea81a917fe582f8d3d70df89142
Rajeshwar singh <43945326+Rajeshwar40@users.noreply.github.com>
2025-12-20 14:39:11 +0530

    Add files via upload

---
+++ js/terminal.js
@@ -0,0 +1,5 @@
+
+const term = document.getElementById("terminal");
+term.addEventListener("click",()=>{
+  term.innerHTML += "<div>> skills</div><div class='out'>Web, API, Network, AD, Red Team</div>";
+});
